7f24095b23bf197fa43b3fffd7b5f97c336b30bc
Joseph Kligel <jkligel@gmail.com>
2025-05-28 19:46:09 -0500

    Finish module 2

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd113590f2c4e45c84657f7868ed98e4f52ebb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc964065fd78c216aa2a1cf18bdbd900eef60d48")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
