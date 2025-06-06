7f907e7c8f2828c419964e0c0c0985378c36d175
Joseph Kligel <jkligel@gmail.com>
2025-06-05 19:30:30 -0500

    Finish lesson 34

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d1b3a42ae9a4f197e6b68dd51a81b74026e5bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eca44274fbc8b08dafe4687eec8c132cfd19938")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
